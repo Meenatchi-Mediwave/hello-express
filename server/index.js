def51513f8f9a054e2c64c31a9a8503a967e686e
Ashish Kurian Thomas <ashish@mindwaveventures.com>
2022-12-15 17:54:14 +0530

    add other endpoints

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const express = require("express");
 
 const app = express();
 
+// get body json
+app.use(express.json());
+
 const books = [
   { id: 1234567890, name: "Harry Potter" },
   { id: 9876543210, name: "IT" },
@@ -12,8 +15,46 @@ const books = [
 const PORT = process.env.PORT || 4567;
 const APP_NAME = process.env.APP_NAME || "api";
 
+// CRUD
+// create read update delete
+
 app.get("/books", (req, res) => {
   return res.send(books);
+});
+
+app.post("/books", (req, res) => {
+  // { name: 'Game of thrones' }
+  if (!req.body.name) {
+    return res.status(400).send({
+      message: "Book must have a name",
+    });
+  }
+  const newBook = {
+    name: req.body.name,
+    id: new Date().getTime(),
+  };
+  books.push(newBook);
+  return res.send(newBook);
+});
+
+app.put("/books/:bookid", (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).send({
+      message: "Book name not passed",
+    });
+  }
+
+  const bookIndex = books.findIndex((b) => b.id === Number(req.params.bookid));
+
+  if (bookIndex === -1) {
+    return res.status(404).send({
+      message: "Book not found",
+    });
+  }
+  books[bookIndex]["name"] = req.body.name;
+  return res.send({
+    message: "Book updated",
+  });
 });
 
 app.get("/books/:bookid", (req, res) => {
@@ -36,6 +77,13 @@ app.delete("/books/:bookid", (req, res) => {
   books.splice(bookIndex, 1);
   return res.send({
     message: "Book removed",
+  });
+});
+
+// not found middleware
+app.use((req, res) => {
+  return res.status(404).send({
+    message: "Route not found",
   });
 });
 
